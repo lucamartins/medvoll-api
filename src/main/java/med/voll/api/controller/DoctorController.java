7b1ef76e8508b42a7209687f5bfc5c856b2056d4
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-05-19 01:57:52 -0300

    feat(doctorController): add get doctors

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.doctor.Doctor;
+import med.voll.api.doctor.DoctorListItemDTO;
 import med.voll.api.doctor.DoctorRepository;
 import med.voll.api.doctor.RegisterDoctorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
@@ -25,5 +25,15 @@ public class DoctorController {
     @Transactional
     void registerDoctor(@RequestBody @Valid RegisterDoctorDTO registerDoctorDTO) {
         doctorRepository.save(new Doctor(registerDoctorDTO));
+    }
+
+    @GetMapping
+    List<DoctorListItemDTO> getDoctors() {
+        var doctors = doctorRepository.findAll();
+
+        return doctors
+                .stream()
+                .map(d -> new DoctorListItemDTO(d.getName(), d.getEmail(), d.getCrm(), d.getSpecialty()))
+                .toList();
     }
 }
