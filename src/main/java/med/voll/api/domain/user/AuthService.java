bbf53a2ea5ef2518ffe20952ec8a0c1f5ac6ddab
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-05-20 23:15:09 -0300

    feat: add spring security config to enable staless

---
+++ src/main/java/med/voll/api/domain/user/AuthService.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
