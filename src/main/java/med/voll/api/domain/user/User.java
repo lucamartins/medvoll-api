6ac56a6df45942aee9c7246857130cfd7b01c3d1
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-05-20 22:46:20 -0300

    feat: add spring security dep and users table

---
+++ src/main/java/med/voll/api/domain/user/User.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String email;
+
+    String password;
+}
